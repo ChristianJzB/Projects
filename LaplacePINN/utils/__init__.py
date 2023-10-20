cbe9452802f633329f6399c3015d40e0dc104784
Christian_JB <C.Jimenez@sms.ed.ac.uk>
2023-10-20 12:13:50 +0100

    Laplace Approx. first attempt

---
+++ LaplacePINN/utils/__init__.py
@@ -0,0 +1,14 @@
+from LaplacePINN.utils.utils import get_nll, validate, parameters_per_layer, invsqrt_precision, _is_batchnorm, _is_valid_scalar, kron, diagonal_add_scalar, symeig, block_diag, expand_prior_precision
+from LaplacePINN.utils.feature_extractor import FeatureExtractor
+from LaplacePINN.utils.matrix import Kron, KronDecomposed
+from LaplacePINN.utils.swag import fit_diagonal_swag_var
+from LaplacePINN.utils.subnetmask import SubnetMask, RandomSubnetMask, LargestMagnitudeSubnetMask, LargestVarianceDiagLaplaceSubnetMask, LargestVarianceSWAGSubnetMask, ParamNameSubnetMask, ModuleNameSubnetMask, LastLayerSubnetMask
+
+
+__all__ = ['get_nll', 'validate', 'parameters_per_layer', 'invsqrt_precision', 'kron',
+		   'diagonal_add_scalar', 'symeig', 'block_diag', 'expand_prior_precision',
+		   'FeatureExtractor',
+           'Kron', 'KronDecomposed',
+		   'fit_diagonal_swag_var',
+		   'SubnetMask', 'RandomSubnetMask', 'LargestMagnitudeSubnetMask', 'LargestVarianceDiagLaplaceSubnetMask',
+		   'LargestVarianceSWAGSubnetMask', 'ParamNameSubnetMask', 'ModuleNameSubnetMask', 'LastLayerSubnetMask']
